2078ec764403fa11cae4bf5f9c1afd3695254e5c
Eran Ifrah <eran@codelite.org>
2023-01-25 00:30:37 +0200

    added initial gdb driver code

--- src/libdapd/drivers/driver_gdb.rs
+++ src/libdapd/drivers/driver_gdb.rs
@@ -1,30 +1,57 @@
 use crate::events::ProcessEventBody;
+#[allow(unused_imports)]
 use crate::libdapd::{drivers::Message, Driver};
 use crate::requests::LaunchRequestArguments;
 use crate::types::ProcessEventStartMethod;
 use std::io::{Error as IoError, ErrorKind as IoErrorKind};
 use std::{error::Error as StdError, result::Result};
 
+#[allow(unused_imports)]
 use crossbeam_channel::{Receiver, Sender};
-use std::thread;
+//use std::thread;
 
 #[allow(dead_code)]
 pub struct DriverGDB {
     /// Used by the contol thread to send commands to gdb
-    in_write: Option<Sender<Message>>,
-    /// Used by gdb to read commands
-    in_read: Option<Receiver<Message>>,
+    debugger: Option<gdb::Debugger>,
 }
 
 impl DriverGDB {
     #[allow(dead_code)]
     pub fn new() -> Self {
-        let (sender, receiver) = crossbeam_channel::unbounded::<Message>();
-        Self {
-            in_write: Some(sender),
-            in_read: Some(receiver),
+        Self { debugger: None }
+    }
+}
+
+impl DriverGDB {
+    fn send_cmd_raw(
+        &mut self,
+        cmd: &str,
+    ) -> Result<gdb::MessageRecord<gdb::ResultClass>, Box<dyn StdError>> {
+        let Some(debugger) = &mut self.debugger else {
+            return Err(Box::new(IoError::new(IoErrorKind::InvalidInput, "gdb is not running")));
+        };
+
+        match debugger.send_cmd_raw(cmd) {
+            Ok(record) => Ok(record),
+            Err(e) => Err(make_error(e)),
         }
     }
+}
+
+fn make_error(gdb_err: gdb::Error) -> Box<dyn StdError> {
+    match gdb_err {
+        gdb::Error::IOError(e) => Box::new(e),
+        gdb::Error::ParseError => Box::new(IoError::new(
+            IoErrorKind::InvalidData,
+            "failed to parse gdb output!",
+        )),
+        gdb::Error::IgnoredOutput => Box::new(IoError::new(IoErrorKind::Other, "output ignored")),
+    }
+}
+
+fn make_error_from_string(io_kind: IoErrorKind, msg: &str) -> Box<dyn StdError> {
+    Box::new(IoError::new(io_kind, msg))
 }
 
 impl Driver for DriverGDB {
@@ -33,10 +60,24 @@ impl Driver for DriverGDB {
         launch_args: &LaunchRequestArguments,
     ) -> Result<ProcessEventBody, Box<dyn StdError>> {
         let Some(exepath) = &launch_args.program else {
-            return Err(Box::new(IoError::new(IoErrorKind::InvalidInput, "missing executable")));
+            return Err(make_error_from_string(IoErrorKind::InvalidInput, "missing executable"));
+        };
+        // always use forward slash
+        let exepath = exepath.replace("\\", "/");
+        let Ok(debugger) = gdb::Debugger::start() else {
+            return Err(make_error_from_string(IoErrorKind::InvalidInput, "failed to start gdb debugger!"));
         };
 
-        thread::spawn(|| {});
+        // Load the executable
+        self.debugger = Some(debugger);
+        let res = self.send_cmd_raw(&format!(r#"-file-exec-and-symbols "{}"#, exepath))?;
+        if res.class != gdb::ResultClass::Done {
+            return Err(make_error_from_string(
+                IoErrorKind::InvalidData,
+                &format!("{:?}", res.content),
+            ));
+        }
+
         Ok(ProcessEventBody {
             name: exepath.clone(),
             system_process_id: Some(-1),
@@ -44,5 +85,27 @@ impl Driver for DriverGDB {
             start_method: Some(ProcessEventStartMethod::Launch),
             pointer_size: None,
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_launch_gdb() -> Result<(), Box<dyn StdError>> {
+        let mut driver = DriverGDB::new();
+        let launch_args = LaunchRequestArguments {
+            no_debug: Some(false),
+            restart_data: None,
+            program: Some(
+                r#"C:\Users\eran\Documents\HellWorldCxx\build-Debug\bin\HellWorldCxx.exe"#
+                    .to_string(),
+            ),
+            args: None,
+            cwd: None,
+            env: None,
+        };
+        let _ = driver.launch(&launch_args)?;
+        Ok(())
     }
 }
